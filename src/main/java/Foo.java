d7f0117e4985c11265a5c53cf261e0f67c6c7829
Anna <30463898+AnnaZK@users.noreply.github.com>
2017-12-15 13:52:35 +0200

    Update Foo.java

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,6 +1,7 @@
 /**
  * @author Anton Chernetskij
  */
+
 public class Foo {
     public static void main(String[] args) {
         System.out.println("Hello world");
@@ -13,16 +14,13 @@ public class Foo {
     public void baz(){
         System.out.println("baz");
     }
-<<<<<<< HEAD
 
     public void gas(){
         System.out.println("gas");
-=======
     
     double result; 
     
     public void gas(){ 
         System.out.println("gas"); 
->>>>>>> ba15960fcbea291830c54f5d38597d105072c233
     }
  }
