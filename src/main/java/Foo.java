89c5b27be58e36b6f20c93a82c3b08402895ac92
german <xbox1388@mail.ru>
2018-01-02 17:00:30 +0200

    branch3 commit

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -3,7 +3,7 @@
  */
 public class Foo {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello ukraine");
     }
 
     public void bar(){
