5a14fdbb7232155558788c2abc66a2234654a9d1
german <xbox1388@mail.ru>
2018-01-08 20:59:08 +0200

    create branch, create maven project from archetype, tested goals

--- mvn_hw/src/main/java/Foo.java
+++ mvn_hw/src/main/java/Foo.java
@@ -4,20 +4,19 @@
 public class Foo {
 
 
-
-
     public static void main(String[] args) {
         System.out.println("Hello ukraine");
     }
 
 
-    public void bar(){
+    public void bar() {
         System.out.println("bar");
     }
 
 
-
-    public void bazzz(){system.out.println("bazzz")}
+    public void bazzz() {
+        System.out.println("bazzz");
+    }
 
     public void money() {
         System.out.println("grivna");
