92586669e8ff033ecaf7a28a37323c9e95fc3b9c
german <xbox1388@mail.ru>
2018-01-08 21:22:17 +0200

    create simple test

---
+++ mvn_hw/src/test/FooTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FooTest {
+
+    @Test
+    public void main() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void bar() {
+    }
+
+    @Test
+    public void bazzz() {
+    }
+
+    @Test
+    public void money() {
+    }
+}
