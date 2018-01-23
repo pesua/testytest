f619a16c055ff6064faf189cddf00943ca692335
vitaly.leontev <vleon2690@gmail.com>
2018-01-23 21:11:19 +0200

    Guava dependency added, String field created and method replaceAll ran in main class

--- homework-maven/src/main/java/MavenClass.java
+++ homework-maven/src/main/java/MavenClass.java
@@ -1,6 +1,8 @@
 public class MavenClass {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        String greeting = "Hello, dawg!";
+        System.out.println(greeting.replaceAll("aw","o"));
     }
 }
